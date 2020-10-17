cd025a29b56fbc66681f2044fda5e9ce073ffcd9
mehrabtoghani <mehrabtoghani@gmail.com>
2020-10-17 21:49:16 +0200

    fix including files.

--- CarryLookAheadFullAdder/FullAdderN.v
+++ CarryLookAheadFullAdder/FullAdderN.v
@@ -1,3 +1,5 @@
+`include "FullAdder.v"
+
 module FullAdderN ( X, Y, CarryIn, Sum, CarryOut );
 	parameter n = 1;
 	input wire [n-1:0] X , Y;
@@ -11,6 +13,9 @@ module FullAdderN ( X, Y, CarryIn, Sum, CarryOut );
 	assign CarryOut = InternalCarry [n];
 
 	generate for (i = 0; i<n ; i = i+1) begin
+			// G[i] = A[i] + B[i]
+			// P[i] = A[i] * B[i]
+			// C[i+1] = G[i] + ( P[i] * C[i] )
 			assign InternalCarry[i+1] = (X[i] & Y[i]) | ((X[i] | Y[i]) & InternalCarry[i]);
 			FullAdder fullAdder(X[i], Y[i], InternalCarry[i], Sum[i], );
 		end
