59c467f01fac3bab35f7713e42322c5587452561
mehrabtoghani <mehrabtoghani@gmail.com>
2020-10-17 12:28:05 +0200

    update one bit fulladder: use 'always'.

--- FullAdder64.v
+++ FullAdder64.v
@@ -1,10 +1,13 @@
 module FullAdder ( X, Y, CarryIn, Sum, CarryOut );
-	input X , Y;
-	input CarryIn;
-	output Sum;
-	output CarryOut;
-	assign Sum = X ^ Y ^ CarryIn;
-	assign CarryOut = (X & Y) | ((X ^ Y) & CarryIn);
+	input wire X , Y;
+	input wire CarryIn;
+	output reg Sum;
+	output reg CarryOut;
+	always @(*)
+		begin
+		assign Sum = X ^ Y ^ CarryIn;
+		assign CarryOut = (X & Y) | ((X ^ Y) & CarryIn);
+		end
 endmodule;
 
 module main;
@@ -14,12 +17,12 @@ module main;
 	wire carryOut;
 	FullAdder fullAdder(.X(x), .Y(y), .CarryIn(carryIn), .Sum(sum), .CarryOut(carryOut));
   	initial
-    	begin
-	#10 
-      	assign x = 1;
-	assign y = 1;
-	assign carryIn = 1;
-      	#20 
-	$display("%0d + %0d + %0d = %0d;  Carry out = %0d", x, y, carryIn, sum, carryOut);
-    	end
+    		begin
+		#10 
+      		assign x = 1;
+		assign y = 1;
+		assign carryIn = 1;
+		#20 
+		$display("%0d + %0d + %0d = %0d;  Carry out = %0d", x, y, carryIn, sum, carryOut);
+    		end
 endmodule;
